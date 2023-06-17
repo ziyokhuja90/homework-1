ad4425e7131ef8d5186b47ce20f50808ed2a3f0c
Husniddin <uzbekfreedom@gmail.com>
2023-06-17 13:50:20 +0500

    homework

---
+++ main.py
@@ -0,0 +1,13 @@
+
+def hisobla(*ism):
+    """
+        bu hisoblash uchun funksiya. sizdan hozir 2 ta son va amalni so'raymiz
+    """
+    
+    umumiy = sum(ism)
+    return umumiy
+
+
+hisobla = hisobla(*range(1,20))
+
+print(hisobla)
