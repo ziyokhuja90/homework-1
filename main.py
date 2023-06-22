efaf89cc64433828201771f6c67c01df5476d612
Husniddin <103229084+mehmonov@users.noreply.github.com>
2023-06-22 14:11:42 +0500

    Update main.py
    shart main.py

--- main.py
+++ main.py
@@ -1,14 +1,2 @@
 
-# bu yangi funksiya
-def hisobla(*ism):
-    """
-        bu hisoblash uchun funksiya. sizdan hozir 2 ta son va amalni so'raymiz
-    """
-    
-    umumiy = sum(ism)
-    return umumiy
-
-
-hisobla = hisobla(*range(1,20))
-
-print(hisobla)
+# kitoblar haqidagi funksiyalar
